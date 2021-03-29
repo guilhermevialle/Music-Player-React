b7d7553a81fe85bc1307b0ba52d74af6faf8c5f1
guilhermevialle <guilhef2@hotmail.com>
2021-03-29 20:02:03 -0300

    Adding files... Bugs fixes, features added...

---
+++ src/App.js
@@ -0,0 +1,355 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./App.scss";
+
+// svgs
+import Repeat from "./svgs/repeat.svg";
+import Previous from "./svgs/previous.svg";
+import Play from "./svgs/play.svg";
+import Next from "./svgs/next.svg";
+import Volume from "./svgs/volume.svg";
+import NoVolume from "./svgs/novolume.svg";
+import LowVolume from "./svgs/lowvolume.svg";
+import Pause from "./svgs/pause.svg";
+import Playlisticon from "./svgs/playlisticon.svg";
+
+// images
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import img9 from "./images/img9.jpg";
+
+// musics
+import music1 from "./musics/music1.mp3";
+import music2 from "./musics/music2.mp3";
+import music3 from "./musics/music3.mp3";
+import music4 from "./musics/music4.mp3";
+import music5 from "./musics/music5.mp3";
+import music6 from "./musics/music6.mp3";
+import music7 from "./musics/music7.mp3";
+import music8 from "./musics/music8.mp3";
+import music9 from "./musics/music9.mp3";
+
+var arrayIndex = 0;
+
+function App() {
+    const [tracks, setTracks] = useState([
+        {
+            autor: "[NCS] Free to use",
+            name: "Art of Silence",
+            songPath: music1,
+            imgPath: img1,
+        },
+        {
+            autor: "Tenno",
+            name: "Vision",
+            songPath: music2,
+            imgPath: img2,
+        },
+        {
+            autor: "Nourish",
+            name: "All that you are is all that i need",
+            songPath: music3,
+            imgPath: img3,
+        },
+        {
+            autor: "ON TOP",
+            name: "the girl next door",
+            songPath: music4,
+            imgPath: img4,
+        },
+        {
+            autor: "Artist",
+            name: "Levi vs Beast Titan Theme Season 4",
+            songPath: music5,
+            imgPath: img5,
+        },
+        {
+            autor: "Bloodbath",
+            name: "Eaten",
+            songPath: music6,
+            imgPath: img6,
+        },
+        {
+            autor: "NCS - No Copyright Songs",
+            name: "The floor is lava",
+            songPath: music7,
+            imgPath: img7,
+        },
+        {
+            autor: "Animes Soundtracks",
+            name: "Reiner and Bertholdt Reveal Theme Season 2",
+            songPath: music8,
+            imgPath: img8,
+        },
+        {
+            autor: "Suicide Silence",
+            name: "You only live once",
+            songPath: music9,
+            imgPath: img9,
+        },
+    ]);
+
+    const [selectedMusic, setSelectedMusic] = useState(tracks[0]);
+
+    var audio = useRef(null);
+    var trackSliderElement = useRef(null);
+    var volumeSlider = useRef(null);
+    var currentTiming = useRef(null);
+    var totalTiming = useRef(null);
+    var playlistInterface = useRef(null);
+    var playSvg = useRef(null);
+
+    var timer;
+
+    const resetSlider = () => {
+        trackSliderElement.current.value = 0;
+    };
+
+    useEffect(() => {
+        const targets = playlistInterface.current.childNodes;
+
+        targets.forEach((target) => {
+            if (target.classList.contains("currentTrack")) {
+                target.classList.remove("currentTrack");
+            }
+        });
+
+        audio.current.play();
+
+        playSvg.current.src = Pause;
+
+        resetSlider();
+
+        targets[arrayIndex].classList.add("currentTrack");
+
+        timer = setInterval(changeMusicSliding, 1000);
+    }, [selectedMusic]);
+
+    const changeMusicSliding = () => {
+        let position = 0;
+
+        if (!isNaN(audio.current.duration)) {
+            position =
+                audio.current.currentTime * (100 / audio.current.duration);
+
+            trackSliderElement.current.value = position;
+        }
+    };
+
+    return (
+        <div className="globalBox">
+            <div className="sendfilesInterface"></div>
+
+            <div className="interface">
+                <div className="playlistBtn">
+                    <img
+                        onClick={() => {
+                            if (
+                                playlistInterface.current.style.height == "0px"
+                            ) {
+                                playlistInterface.current.style.height =
+                                    "550px";
+                                return;
+                            }
+
+                            playlistInterface.current.style.height = "0px";
+                        }}
+                        src={Playlisticon}
+                    />
+                </div>
+                <div className="musicImg">
+                    <img src={selectedMusic.imgPath} />
+                </div>
+                <audio
+                    onDurationChange={(e) => {
+                        totalTiming.current.innerHTML =
+                            parseInt((audio.current.duration / 60) % 60) +
+                            ":" +
+                            parseInt(audio.current.duration % 60);
+                    }}
+                    onEnded={() => {
+                        if (
+                            arrayIndex ==
+                            playlistInterface.current.childNodes.length - 1
+                        ) {
+                            console.log(
+                                "Limpando intervalo... Cancelando próxima música."
+                            );
+                            clearInterval(timer);
+                            return;
+                        } else {
+                            arrayIndex++;
+                            setSelectedMusic(tracks[arrayIndex]);
+                        }
+                    }}
+                    onTimeUpdate={(e) => {
+                        var s = parseInt(e.target.currentTime % 60);
+                        var m = parseInt((e.target.currentTime / 60) % 60);
+                        currentTiming.current.innerHTML = m + ":" + s;
+                    }}
+                    ref={audio}
+                    src={selectedMusic.songPath}
+                ></audio>
+                <div className="info">
+                    <h3>
+                        <h3>{selectedMusic.name}</h3>
+                    </h3>
+                    <h4>{selectedMusic.autor} </h4>
+                </div>
+                <div className="slider">
+                    <input
+                        onChange={(e) => {
+                            e.preventDefault();
+                            var sliderPosition =
+                                audio.current.duration * (e.target.value / 100);
+
+                            if (sliderPosition == audio.current.duration) {
+                                console.log("ta igual");
+                            }
+                            audio.current.currentTime = sliderPosition;
+                        }}
+                        max="100"
+                        min="0"
+                        type="range"
+                        name="slider"
+                        id="slider"
+                        ref={trackSliderElement}
+                    />
+                    <div>
+                        <p ref={currentTiming}></p>
+                        <p ref={totalTiming}></p>
+                    </div>
+                </div>
+
+                <div className="controls">
+                    <button
+                        onClick={(e) => {
+                            if (audio.current.loop) {
+                                audio.current.loop = false;
+                                e.currentTarget.childNodes[0].style.transform =
+                                    "rotateZ(0deg)";
+                                return;
+                            }
+
+                            e.currentTarget.childNodes[0].style.transform =
+                                "rotateZ(-90deg)";
+                            audio.current.loop = true;
+                        }}
+                    >
+                        <img src={Repeat} />
+                    </button>
+                    <button
+                        onClick={() => {
+                            if (arrayIndex == 0) {
+                                return;
+                            }
+
+                            arrayIndex--;
+                            setSelectedMusic(tracks[arrayIndex]);
+                        }}
+                    >
+                        <img src={Previous} />
+                    </button>
+                    <button
+                        onClick={(e) => {
+                            if (!audio.current.paused) {
+                                audio.current.pause();
+                                e.currentTarget.childNodes[0].src = Play;
+                                return;
+                            }
+
+                            e.currentTarget.childNodes[0].src = Pause;
+                            audio.current.play();
+
+                            totalTiming.current.innerHTML =
+                                parseInt((audio.current.duration / 60) % 60) +
+                                ":" +
+                                parseInt(audio.current.duration % 60);
+                        }}
+                    >
+                        <img src={Play} ref={playSvg} />
+                    </button>
+                    <button
+                        onClick={() => {
+                            if (arrayIndex == tracks.length - 1) {
+                                return;
+                            }
+
+                            arrayIndex++;
+                            setSelectedMusic(tracks[arrayIndex]);
+                        }}
+                    >
+                        <img src={Next} />
+                    </button>
+                    <button
+                        onClick={(e) => {
+                            if (audio.current.volume == 0) {
+                                audio.current.volume = 0.2;
+                                volumeSlider.current.value = 20;
+                                e.currentTarget.childNodes[0].src = Volume;
+                                return;
+                            }
+
+                            audio.current.volume = 0;
+                            volumeSlider.current.value = 0;
+                            e.currentTarget.childNodes[0].src = NoVolume;
+                        }}
+                    >
+                        <img src={Volume} />
+                    </button>
+                    <input
+                        ref={volumeSlider}
+                        type="range"
+                        name="vol"
+                        id="vol"
+                        max="100"
+                        min="0"
+                        onChange={(e) => {
+                            audio.current.volume = e.target.value / 100;
+                        }}
+                    />
+                </div>
+            </div>
+
+            <div className="aroundPlaylistInterface">
+                <div className="title">
+                    <h4>Playlist</h4>
+                </div>
+
+                <div className="playlistInterface" ref={playlistInterface}>
+                    {tracks.map((item) => {
+                        return (
+                            <div
+                                className="trackListed"
+                                onClick={(e) => {
+                                    const index = [
+                                        ...e.currentTarget.parentNode
+                                            .childNodes,
+                                    ].indexOf(e.currentTarget);
+
+                                    arrayIndex = index;
+                                    setSelectedMusic(item);
+                                }}
+                            >
+                                <div className="trackImg">
+                                    <img src={item.imgPath} />
+                                </div>
+                                <div className="trackInfo">
+                                    <h4>{item.name}</h4>
+                                    <p>{item.autor}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
